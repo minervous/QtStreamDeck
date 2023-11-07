8d6f20cd1edd6616375c13e11883a6060717013d
GooRoo <sergey.olendarenko@gmail.com>
2023-11-07 18:26:22 +0100

    fix: Remove obsolete files

--- extensions/minervous.streamdeck/src/StreamDeckPlugin.hpp
+++ extensions/minervous.streamdeck/src/StreamDeckPlugin.hpp
@@ -2,16 +2,47 @@
 
 #include <QtQml/QQmlEngineExtensionPlugin>
 
+#include "minervous/streamdeck/Device.hpp"
+#include "minervous/streamdeck/DeviceEmulator.hpp"
+#include "minervous/streamdeck/DeviceManager.hpp"
+
 namespace minervous::streamdeck
 {
-
 	class StreamDeckPlugin: public QQmlEngineExtensionPlugin
 	{
 		Q_OBJECT
-		Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
+		Q_PLUGIN_METADATA(IID QQmlEngineExtensionInterface_iid)
+	};
 
-	public:
-		void registerTypes(const char * uri);
+	struct StreamDeckQml
+	{
+		Q_GADGET
+		QML_FOREIGN(minervous::streamdeck::Device)
+		QML_NAMED_ELEMENT(StreamDeck)
+		// QML_EXTENDED(minervous::streamdeck::Device)
+	};
+
+	struct StreamDeckEmulatorQml
+	{
+		Q_GADGET
+		QML_FOREIGN(minervous::streamdeck::DeviceEmulator)
+		QML_NAMED_ELEMENT(StreamDeckEmulator)
+		// QML_EXTENDED(minervous::streamdeck::Device)
+	};
+
+	struct StreamDeckTypeQml
+	{
+		Q_GADGET
+		QML_FOREIGN(minervous::streamdeck::DeviceTypeGadget)
+		QML_NAMED_ELEMENT(StreamDeckType)
+	};
+
+	struct StreamDeckManagerQml
+	{
+		Q_GADGET
+		QML_FOREIGN(minervous::streamdeck::DeviceManager)
+		QML_SINGLETON
+		QML_NAMED_ELEMENT(StreamDeckManager)
 	};
 
 }  // namespace minervous::streamdeck
